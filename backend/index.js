eed3f23538e19f1be9aee2652bdaead69379bb27
cn-gourav <mrgusain558@gmail.com>
2025-12-05 20:25:34 +0530

    Update :: backend run

--- backend/index.js
+++ backend/index.js
@@ -10,12 +10,12 @@ const { HoldingsModel } = require("./model/HoldingsModel");
 const { PositionsModel } = require("./model/PositionsModel");
 const { OrdersModel } = require("./model/OrdersModel");
 
-// const PORT = process.env.PORT || 3002;
-// const uri = process.env.MONGO_URL;
+const PORT = process.env.PORT || 3002;
+const uri = process.env.MONGO_URL;
 
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 app.use(bodyParser.json());
 
 // app.get("/addHoldings", async (req, res) => {
@@ -188,30 +188,30 @@ app.use(bodyParser.json());
 // });
 
 app.get("/allHoldings", async (req, res) => {
-  let allHoldings = await HoldingsModel.find({});
-  res.json(allHoldings);
+    let allHoldings = await HoldingsModel.find({});
+    res.json(allHoldings);
 });
 
 app.get("/allPositions", async (req, res) => {
-  let allPositions = await PositionsModel.find({});
-  res.json(allPositions);
+    let allPositions = await PositionsModel.find({});
+    res.json(allPositions);
 });
 
 app.post("/newOrder", async (req, res) => {
-  let newOrder = new OrdersModel({
-    name: req.body.name,
-    qty: req.body.qty,
-    price: req.body.price,
-    mode: req.body.mode,
-  });
+    let newOrder = new OrdersModel({
+        name: req.body.name,
+        qty: req.body.qty,
+        price: req.body.price,
+        mode: req.body.mode,
+    });
 
-  newOrder.save();
+    newOrder.save();
 
-  res.send("Order saved!");
+    res.send("Order saved!");
 });
 
 app.listen(PORT, () => {
-  console.log("App started!");
-  mongoose.connect(uri);
-  console.log("DB started!");
+    console.log("App started!");
+    mongoose.connect(uri);
+    console.log("DB started!");
 });
